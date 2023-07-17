10e2e919ee172bee5ea0c92cb57876b06e4a864d
NoeLac <noe.lacaille@efrei.net>
2023-07-17 23:18:53 +0200

    v0.1

---
+++ src/script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('header nav ul li a');
+
+    links.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const href = this.getAttribute('href');
+            const target = document.querySelector(href);
+            const offsetTop = target.offsetTop;
+            const duration = 1000; // Durée de l'animation en millisecondes
+            scrollSmoothly(offsetTop, duration);
+        });
+    });
+
+    function scrollSmoothly(targetPosition, duration) {
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+        let start = null;
+
+        function step(timestamp) {
+            if (!start) start = timestamp;
+            const progress = timestamp - start;
+            const percentage = Math.min(progress / duration, 1);
+            const easing = easeInOutCubic(percentage);
+            window.scrollTo(0, startPosition + distance * easing);
+            if (progress < duration) {
+                window.requestAnimationFrame(step);
+            }
+        }
+
+        window.requestAnimationFrame(step);
+    }
+
+    // Fonction d'interpolation pour obtenir un défilement lent
+    function easeInOutCubic(t) {
+        return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+    }
+});
+
+/******************************************************/
